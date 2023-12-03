44e8c18486cfc31c3c64176dc5564c90105c701b
joelcena <joelcena.47@gmail.com>
2023-12-03 22:46:21 +0200

    footer

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -6,13 +6,17 @@ const Market = () => {
       <div className='lg:w-4/5 h-auto flex flex-col space-y-8 text-white'>
         <h2 className='text-4xl font-bold'>Market Update</h2>
         <div className='w-full h-full'>
-          <table className='w-auto h-full text-center text-2xl border-separate spacing-y-2 bg-gradient-to-r from-first to-title rounded-lg'>
+          <table className='w-full h-full text-center text-2xl border-separate spacing-y-2 bg-gradient-to-r from-first to-title rounded-lg'>
             <thead>
-              <tr>
+              <tr className='flex flex col justify-around items-center'>
                 <td className='text-left font-semibold w-[20%] p-2'>Coin</td>
                 <td className='font-semibold w-[14%] pb-2'>Price</td>
-                <td className='font-semibold w-[14%] pb-2'>24h Change</td>
-                <td className='font-semibold w-[10%] pb-2'>Market Cap</td>
+                <td className='font-semibold w-[14%] pb-2 hidden md:block lg:text-xl md:text-base'>
+                  24h Change
+                </td>
+                <td className='font-semibold w-[14%] pb-2 hidden md:block lg:text-xl md:text-base'>
+                  Market Cap
+                </td>
               </tr>
             </thead>
           </table>

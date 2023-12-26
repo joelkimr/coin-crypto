07eb2b66123f9101cb65ee6654dae405402039e8
joelcena <joelcena.47@gmail.com>
2023-12-26 16:14:06 +0200

    ft-loaders

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 const Hero = () => {
   const [data, setData] = useState([]);
   const [coinsLoad, setCoinsLoad] = useState(true);
+  const [showPrice, setShowPrice] = useState(false);
 
   const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false
   `;
@@ -18,21 +19,21 @@ const Hero = () => {
   }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error("Error!");
-        }
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    if (data.length === 0) {
+      fetchData();
+    }
+  }, [data]);
 
-    fetchData();
-  }, [url]);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Error!");
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {}
+  };
 
   return (
     <div>
@@ -49,7 +50,7 @@ const Hero = () => {
                 className='md:block hidden'
               />
             </a>
-            <div className='text-center uppercase flex flex-col'>
+            <div className='text-center uppercase flex flex-col pt-20'>
               <div>
                 <p className='text-white lg:text-7xl text-5xl font-bold'>
                   track and trade
@@ -71,41 +72,52 @@ const Hero = () => {
           </div>
           <a
             href='#'
-            className='text-[1.5rem] w-[15rem] h-[4rem] text-center pt-2 bg-gradient-to-t from-first to-title text-white rounded-full transition md:hidden block font-bold'>
+            onClick={() => setShowPrice(!showPrice)}
+            className='text-[1.5rem] w-[15rem] h-[4rem] min-h-[4rem] text-center pt-2 bg-gradient-to-t from-first to-title text-white rounded-full transition md:hidden block font-bold'>
             See Prices
           </a>
           <div
             onLoad={() => setCoinsLoad(false)}
-            className='flex-row gap-x-28 md:block hidden'>
-            {coinsLoad && (
-              <span>{/* <Spinner className='h-10 w-10' /> */}</span>
+            className={`flex md:flex-row lg:gap-x-20 md:gap-x-14 gap-x-3 m-9 ${
+              showPrice ? " " : " hidden md:flex"
+            }`}>
+            {data.length > 0 ? (
+              data.map((item) => (
+                <Link href={`/coin/${item.id}`} key={item.id}>
+                  <img
+                    src={item?.image}
+                    alt={item?.name || "coins"}
+                    // width={80}
+                    // height={80}
+                    className='ml-5 md:w-20 md:h-20 w-12 h-12'
+                  />
+                  <p className='lg:text-2xl md:text-lg text-base font-bold text-white mt-4'>
+                    {item?.name}
+                    <span
+                      className={
+                        "  " +
+                        (item.price_change_percentage_24h <= 0
+                          ? "text-red"
+                          : "text-green")
+                      }>
+                      {item?.price_change_percentage_24h?.toFixed(2) + "%"}
+                    </span>
+                  </p>
+                  <p className='md:text-2xl text-base text-white text-center font-bold'>
+                    {"$ " + numberWithCommas(item.current_price?.toFixed(2))}
+                  </p>
+                </Link>
+              ))
+            ) : (
+              <div className='flex flex-row lg:gap-x-20 md:gap-x-14 gap-x-4'>
+                {Array.from({ length: 4 }).map((_item, i) => (
+                  <div
+                    className='w-16 h-16 bg-title/50 animate-pulse rounded-full'
+                    key={i}
+                  />
+                ))}
+              </div>
             )}
-            {data.map((item) => (
-              <Link href={`/coin/${item.id}`} key={item.id}>
-                <img
-                  src={item?.image}
-                  alt={item?.name}
-                  width={100}
-                  height={100}
-                  className='ml-5'
-                />
-                <p className='text-2xl font-bold text-white mt-4'>
-                  {item?.name}
-                  <span
-                    className={
-                      "  " +
-                      (item.price_change_percentage_24h <= 0
-                        ? "text-red"
-                        : "text-green")
-                    }>
-                    {item?.price_change_percentage_24h?.toFixed(2) + "%"}
-                  </span>
-                </p>
-                <p className='text-2xl text-white text-center font-bold'>
-                  {"$ " + numberWithCommas(item.current_price?.toFixed(2))}
-                </p>
-              </Link>
-            ))}
           </div>
         </div>
       </div>
